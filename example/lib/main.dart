256f153e62a3442d733c4fe23ea6e7f73d526918
mirine vincent <vmirine@vulog.com>
2021-04-09 16:16:46 +0200

    null_safety

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:flurry/flurry.dart';
-import 'package:flutter/material.dart';
 import 'dart:async';
+
+import 'package:flurry_event/flurry.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/MIRINE/Projects/flutter_sdk/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/MIRINE/Projects/flutter/temp/flurry_plugin_flutter/example"
+export "FLUTTER_APPLICATION_PATH=/Users/MIRINE/Projects/flutter/temp/flurry_plugin_flutter"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_BUILD_NAME=0.1.3"
+export "FLUTTER_BUILD_NUMBER=0.1.3"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
